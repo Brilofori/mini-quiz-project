621776e1d2ec667a298655e335b05dcbc81030bf
Brilofori <bril.ofori@gmail.com>
2025-11-03 00:54:13 +1100

    Delete print(' 1').py

--- print(' 1').py
+++
@@ -1,8 +0,0 @@
-
-#password bopundaries 
-#must be 12-14 chracters long 
-#must be able to process symbols 
-#output weak,medium strong according to input 
-
-password = str(input("input password: "))
-print(type(password))
